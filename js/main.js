7650a3ce562ebe266ded2e144367078f44c3affd
iramilyukova <60115026+iramilyukova@users.noreply.github.com>
2020-06-04 23:08:25 +0300

    Дописываю код первого задания

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-var COUNT_USERS = 8,
-  
-var TITLE = ['Уютное гнездышко для молодоженов', 'Милая, уютная квартирка в центре Токио'],
+var COUNT_USERS = 8;
+var TITLE = ['Уютное гнездышко для молодоженов', 'Милая, уютная квартирка в центре Токио', 'Большая уютная квартира'];
 var PriseLimit = {
   MIN: 1000,
   MAX: 1000000
@@ -29,10 +28,13 @@ var PIN_WIDTH = 56;
 var TIMES = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-
+var DESCRIPTION = ['Маленькая чистая квартира на краю города', 'Большая квартира из трех комнат в центре города', 'Однушка у парка'];
 var map = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
-var mapPinTemplate = template.content.querySelector('.map__pin');
+var mapPinTemplate = document.querySelector('.#pin').textContent.querySelector('.map-pin');
+
+ //Удаляем неактивный класс у метки
+ document.querySelector('.map').classList.remove('map--faded');
 
 // Функция, возвращающая случайное число в диапазоне
 var getRandomValue = function (min, max) {
@@ -48,49 +50,47 @@ var getRandomItem = function(items) {
   return randomItem;
   }
 
-//Создаем массив маркеров
-var getMarks = function(count) {
-    var marks = [];
-
-    for(var i = 0; i < count; i++){
-        var mark = getMark();
-        marks.push(mark);
-    }
-
-    return marks;
-};
+  // Функция, возвращающая случайную длину массива
+var getRandomItems = function(count, items) {
+  var randomItem = [];
+  
+  for (var i = 0; i < count; i++) {
+    var randomItem = getRandomValue(items);
+    items.push(randomItem);
+  }
+  return randomItems;
+  }
 
 // Работаем с массивом аватарок
-vat generateAvatars = function() {
+var generateAvatar = function() {
   var listAvatars = [];
 
   for (var i = 1; i < COUNT_USERS + 1; i++) {
     if (i <= 8) {
       i = '0' + i;
     };
-    var avatars = 'img/avatars/user' + i + '.png';
-    listAvatars.push(avatars);
-  }
-  return listAvatars;
+    return 'img/avatars/user' + 0 + index + '.png';
 };
 
 // Функция, возвращающаая массив объектов объявлений
-var getMark = function() {
+var getMark = function(index) {
 
   for (var i = 0; i < COUNT_USERS; i++) {
     var mark = {
-      avatar: generateAvatars(),
-      title: '',
-      address: (600+ ', ' + 350),//строка, адрес предложения
-      price: getRandomValue(),
-      rooms: getRandomValue(),
+      author: {
+        avatar: generateAvatar(index),
+      },
+      title: getRandomItem(TITLE),
+      address: '600, 350', //строка, адрес предложения
+      price: getRandomValue(PriseLimit.MIN, PriseLimit.MAX),
+      rooms: getRandomValue(RoomLimit.MIN, RoomLimit.MAX),
       type: getRandomItem(TYPES),
       guests: getRandomValue(GuestLimit.MIN, GuestLimit.MAX),
-      checkin: getRandomItem(TIME),
-      checkout: getRandomItem(TIME),
-      features: [], //массив строк случайной длины из ниже предложенных
-      description: '',//строка с описанием
-      photos: [], //массив строк случайной длины, содержащий адреса фотографий
+      checkin: getRandomItem(TIMES),
+      checkout: getRandomItem(TIMES),
+      features: getRandomItems(4, FEATURES), //массив строк случайной длины из ниже предложенных
+      description: getRandomItem(DESCRIPTION),//строка с описанием
+      photos: getRandomItems(3, PHOTOS), //массив строк случайной длины, содержащий адреса фотографий
       location: {
             y: getRandomValue(PIN_HEIGHT, PIN_WIDTH),
             x: getRandomValue(PinLimit.MIN_X, PinLimit.MAX_X)
@@ -101,40 +101,38 @@ var getMark = function() {
     return mark;
 }
 
-debugger;
-var marks = getMarks(8);
-debugger;
+//Создаем массив маркеров
+var getMarks = function(count) {
+  var marks = [];
 
-//Удаляем неактивный класс у метки
-  var activePins = function () {
-    var mapFaded = map.querySelector('.map--faded');
-    if (mapFaded) () {
-      map.classList.remove('map--faded');
-    }
-  };
+  for(var i = 0; i < count; i++){
+      var mark = getMark(i);
+      marks.push(mark);
+  }
 
-  //Второй вариант удаления неактивного класса у метки
-  //document.querySelector('.map').classList.remove('map--faded');
-
-// var name = 'bungalo';
-
-// var title = document.querySelector('.notice__title');
-// title.innerText = name;
+  return marks;
+};
 
 //Отрисовываем метки на карте 
-var renderPoints = function(mark) {
-  var mapPoint = buttonTemplate.cloneNode(true);
+var getMarkFragment = function(mark) {
+  var mapPoint = mapPinTemplate.cloneNode(true);
   mapPoint.style.left = mark.location.x + 'px';
-  mapPoint.style.left = mark.location.y + 'px';
+  mapPoint.style.top = mark.location.y + 'px';
   mapPoint.querySelector('img').src = mark.author.avatar;
   
-    return mark;
+    return mapPoint;
   }
+
   //Записываем все метки во fragment
-  var getPinsFragment = function (mark) {
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < mark.length; i++) {
-    fragment.appendChild(renderPoints(mark[i]));
+  var renderMarks = function(marks) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < marks.length; i++) {
+      fragment.appendChild(renderPoints(marks[i]));
+    }
+    mapPins.appendChild(fragment);
   }
-}
-  mapPins.appendChild(fragment);
+
+  debugger;
+  var marks = getMarks(8);
+
+  renderMarks(marks);
